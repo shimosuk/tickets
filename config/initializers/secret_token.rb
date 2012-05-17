bd131d9e7f74039b24a1edcc66512c17be5d853e
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-05-17 22:04:50 +0900

    First Commit
    rails new tickets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = 'd8292c4750eb58dc9c0c650c4d95f306deed7a7be270d2a1ab334e49d569552f1d6797f1b4e0ed3eb8ff448351aa3e6a8cee4e0d5e4be465542e55c4fe2f1092'

62dafb068b45eb0f784637410a4c602f63af9f16
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-06-14 13:26:03 +0900

    欠席した3回目の会のおさらい

---
+++ spec/models/ticket_spec.rb
@@ -0,0 +1,23 @@
+ #coding: utf-8
+ require 'spec_helper'
+ 
+ describe Ticket do
+ 
+ describe 'validation' do
+ before do
+ @ticket = Ticket.new(name: 'akiinyo', seat_id_seq: 'A-7', address: 'Tokyo', price_paid: '3000', email_address: 'akiinyo@example.com')
+ end
+ 
+ subject { @ticket }
+ 
+ context '名前が未入力の場合' do
+ before { @ticket.name = '' }
+ it { should_not be_valid }
+ end
+ 
+ context '名前を入力した場合' do
+ before { @ticket.name = 'akiinyo' }
+ it { should be_valid }
+ end
+ end
+ end

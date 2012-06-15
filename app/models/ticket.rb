42e7743c7381cfa1479e35cd86c7206720f91d05
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-06-15 21:48:29 +0900

    tickets.rbにカラムを追加
    エラーメッセージの変化に注目

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,4 @@
 class Ticket < ActiveRecord::Base
-  attr_accessible :address, :email_address, :name, :price_paid, :seat_id_seq
+  attr_accessible :address, :email_address, :name, :price_paid, :seat_id_seq , :phone_number
   validates :name, presence: true
 end
